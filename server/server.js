08d828ca2a1dbe25b0d9bcc3867d7d335d121e15
LMB.osto <lokmaneboutiba@gmail.com>
2023-06-24 16:09:28 +0100

    start projetc

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+console.log("start");
+
+const app = express();
+const PORT = process.env.PORT ?? 4000;
+
+//? middlewear
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json);
+app.listen(PORT, () => console.log(`start server http://localhost:${PORT}`));
